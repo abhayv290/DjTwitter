61b8d2214018a35b5a34a93d87dc98ead358437d
Abhay Vishwakarma <133063358+abhayv290@users.noreply.github.com>
2024-11-12 09:55:48 +0530

    Update build.sh

--- djX/build.sh
+++ djX/build.sh
@@ -11,6 +11,6 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER]];
+if [[ $CREATE_SUPERUSER]]
 then 
     python manage.py createsuperuser --no-input --email "$SUPERUSER_EMAIL"
